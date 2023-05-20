aac4814ab847d9a441deab9a7e25c31df8ca8cdf
siddhant_prakash <siddhantprakash121@gmail.com>
2023-05-20 18:48:03 +0530

    modified P3

--- Pattern/P3.java
+++ Pattern/P3.java
@@ -6,15 +6,19 @@ public class P3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
+        int sp = n-1;
+        int st = 1;
         for(int i = 1; i <= n; i++){
-            for(int j = n; j > i; j--){
+            for(int k = 1; k <= sp; k++){
                 System.out.print("\t");
             }
-            for(int k = 1; k <= i; k++){
+
+            for(int k = 1; k <= st; k++){
                 System.out.print("*\t");
             }
             System.out.println();
+            sp--;
+            st++;
         }
 
     }
