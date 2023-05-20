7f551aa89d484385a134ae009b9a887c33f1c42d
siddhant_prakash <siddhantprakash121@gmail.com>
2023-05-20 18:31:40 +0530

    added patterns

--- Pattern/P2.java
+++ Pattern/P2.java
@@ -2,11 +2,11 @@ package Pattern;
 
 import java.util.Scanner;
 
-public class P1 {
+public class P2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for(int i = 1; i <= n; i++){
+        for(int i = n; i >= 1; i--){
             for(int j = 1; j <= i; j++){
                 System.out.print("*\t");
             }
--- Pattern/P3.java
+++ Pattern/P3.java
@@ -2,15 +2,20 @@ package Pattern;
 
 import java.util.Scanner;
 
-public class P1 {
+public class P3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+
         for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= i; j++){
+            for(int j = n; j > i; j--){
+                System.out.print("\t");
+            }
+            for(int k = 1; k <= i; k++){
                 System.out.print("*\t");
             }
             System.out.println();
         }
+
     }
 }
