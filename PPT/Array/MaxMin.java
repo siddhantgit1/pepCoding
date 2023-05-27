718a99cebb45607f607e00db1c82b62a63eab1d9
siddhant_prakash <siddhantprakash121@gmail.com>
2023-05-27 12:42:23 +0530

    Added array problems
    some description

---
+++ PPT/Array/MaxMin.java
@@ -0,0 +1,15 @@
+package PPT.Array;
+
+import java.util.Scanner;
+
+public class MaxMin {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.next();
+        for(int i = 0; i < t; i++){
+            String a = sc.nextLine();
+            
+        }
+    }
+}
