2464b5c311b3fe9b22675d3ced0f9a767a65ba13
siddhant_prakash <siddhantprakash121@gmail.com>
2023-05-30 20:53:16 +0530

    exit point

---
+++ Matrix/ExitPoint.java
@@ -0,0 +1,12 @@
+package Matrix;
+
+import java.util.Scanner;
+
+public class ExitPoint {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+    }
+    
+    
+}
